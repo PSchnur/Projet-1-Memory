e6876b92e8c620fef3280f9abe79073d4b49c855
JonathanGab <jogaborit@hotmail.com>
2021-03-19 21:08:41 +0100

    debut du js de la fonction chronometre

---
+++ TestChrono.js
@@ -0,0 +1,26 @@
+let startChrono = document.getElementById('startChrono');
+let stopChrono = document.getElementById('stopChrono');
+let chrono = document.getElementById('chrono');
+let timeoutId;
+let intervalId;
+
+let dec = 0;
+let sec = 0;
+let min = 0;
+chrono.innerHTML = min + ' :' + sec + ' . ' + dec;
+
+startChrono.addEventListener('click', timer);
+stopChrono.addEventListener('click', stopTimer);
+
+
+function timer(){
+    intervalId = setInterval(function(){
+        chrono.innerHTML =  min + ' : ' + sec + ' . ' + dec;
+        dec += 1;
+        if(dec >= 10){dec = 0; sec += 1;}
+        if(sec >= 60){sec = 0; min += 1;}
+    }, 100)
+}
+function stopTimer(){
+    clearInterval(intervalId);
+}
